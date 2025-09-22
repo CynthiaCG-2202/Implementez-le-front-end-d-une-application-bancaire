07fc31e49184a011a67cd787092212a82ea09b56
CynthiaCG-2202 <cynthiacrosagravez@gmail.com>
2025-09-22 11:10:00 +0200

    Ajout du header

--- argentbankreact/src/Components/Header.jsx
+++ argentbankreact/src/Components/Header.jsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
 import "../styles/main.css"; 
+import logo from "../assets/Images/argentBankLogo.png";
 
 function Header() {
   return (
     <nav className="main-nav">
+      {/* Logo cliquable */}
       <Link className="main-nav-logo" to="/">
         <img
           className="main-nav-logo-image"
-          src="./assets/Images/argentBankLogo.png"
+          src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
+
+      {/* Liens de navigation */}
       <div>
-        <Link className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
+        <Link className="main-nav-item" to="/sign-in">
+          <i className="fa fa-user-circle"></i> Sign In
         </Link>
       </div>
     </nav>
